4478b57c755f1332eddc1cbcb0c70ec982262a18
lexo borashvili <lexoborashvili999@gmail.com>
2018-03-11 12:37:38 +0400

    refactor changes

--- src/main/java/ge/mziuri/dao/StaffDAOImpl.java
+++ src/main/java/ge/mziuri/dao/StaffDAOImpl.java
@@ -1,6 +1,7 @@
 package ge.mziuri.dao;
 
 import ge.mziuri.model.Staff;
+import ge.mziuri.model.StaffStatus;
 import ge.mziuri.util.DataBaseConnector;
 
 import java.sql.Connection;
@@ -33,20 +34,18 @@ public class StaffDAOImpl implements StaffDAO {
 
     @Override
     public void editStaff(Staff staff, Connection con) throws Exception {
-        PreparedStatement pstmt1 = con.prepareStatement( "Update staff SET pnumber =? , pnumber2 =? , mail = ? , pass = ?  Where id=? ");
-        PreparedStatement pstmt2 = con.prepareStatement("SELECT * FROM staff WHERE id=?");
-        pstmt2.setInt(1,staff.getId());
-        pstmt1.setInt(5,staff.getId());
-        ResultSet rs = pstmt2.executeQuery();
-        rs.next();
-        if(staff.getMainPhoneNumber().isEmpty()) pstmt1.setInt(1, rs.getInt("mainphonenumber"));
-            else pstmt1.setString(1,staff.getMainPhoneNumber());
-        if(staff.getAdditionalPhoneNumber().isEmpty() ) pstmt1.setInt(2, rs.getInt("additionalphonenumber"));
-            else pstmt1.setString(2,staff.getAdditionalPhoneNumber());
-        if(staff.getEmail().isEmpty()) pstmt1.setString(3, rs.getString("email"));
-            else pstmt1.setString(3, staff.getEmail());
-        if(staff.getPassword().isEmpty()) pstmt1.setString(4,rs.getString("password"));
-            else pstmt1.setString(4,staff.getPassword());
+        PreparedStatement pstmt = con.prepareStatement( "Update staff SET firstname = ?, lastname = ? , main_phone_number =? , additional_phone_number =? , email = ? , password = ? , staff_status = ?  Where id=? ");
+
+        pstmt.setString(1,staff.getFirstname());
+        pstmt.setString(2,staff.getLastname());
+        pstmt.setString(3,staff.getMainPhoneNumber());
+        pstmt.setString(4,staff.getAdditionalPhoneNumber());
+        pstmt.setString(5, staff.getEmail());
+        pstmt.setString(6,staff.getPassword());
+        pstmt.setString(7,staff.getStaffStatus().name());
+        pstmt.executeLargeUpdate();
+        pstmt.close();
+        con.close();
     }
 
     @Override
@@ -58,16 +57,20 @@ public class StaffDAOImpl implements StaffDAO {
         int ID = rs.getInt("id");
         String name =rs.getString("firstname");
         String surname =rs.getString("lastname");
-        String pnumber1=rs.getString("mainphonenumber");
-        String pnumber2=rs.getString("additionalphonenumber");
+        String mainphonenumber=rs.getString("mainphonenumber");
+        String additionalphonenumber=rs.getString("additionalphonenumber");
         String mail = rs.getString("email");
+        String staffstatus = rs.getString("staff_status");
+        StaffStatus staffstatus1 = StaffStatus.valueOf(staffstatus);
+
         Staff staff = new Staff();
         staff.setId(ID);
         staff.setEmail(mail);
         staff.setFirstname(name);
         staff.setLastname(surname);
-        staff.setMainPhoneNumber(pnumber1);
-        staff.setAdditionalPhoneNumber(pnumber2);
+        staff.setMainPhoneNumber(mainphonenumber);
+        staff.setAdditionalPhoneNumber(additionalphonenumber);
+        staff.setStaffStatus(staffstatus1);
         staffs.add(staff);
         }
         pstmt.close();
