2840fb8206d114face6eed9d9da370d8897881ba
LEXO <lexoborashvili999@gmail.com>
2018-03-14 17:49:20 +0400

    Gamartuli StaffDAO

---
+++ src/main/java/ge/mziuri/dao/DAOTest.java
@@ -0,0 +1,81 @@
+package ge.mziuri.dao;
+
+import ge.mziuri.model.Staff;
+import ge.mziuri.model.StaffStatus;
+import ge.mziuri.util.DataBaseConnector;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static ge.mziuri.util.random.RandomTextGenerator.generateRandomString;
+public class DAOTest {
+
+public static void main(String[]args)throws Exception{
+    //TestAddStaff();
+    //TestGetallstaff();
+    //TestEditstaff();
+    TestDeleteStaff();
+        }
+
+    private static  StaffDAO staffDAO = new StaffDAOImpl();
+
+    public static void   TestAddStaff() throws Exception{try {
+        Scanner scanner = new Scanner(System.in);
+        Staff staff = new Staff();
+        staff.setFirstname(scanner.nextLine());
+        staff.setLastname(scanner.nextLine());
+        staff.setEmail(scanner.nextLine());
+        staff.setMainPhoneNumber(scanner.nextLine());
+        staff.setAdditionalPhoneNumber(scanner.nextLine());
+        staff.setPassword(generateRandomString(5));
+        staff.setStaffStatus(StaffStatus.ADMIN);
+        Connection con = DataBaseConnector.getConnection();
+        staffDAO.addStaff(staff, con);
+        con.close();
+    } catch (Exception ex){
+        ex.printStackTrace();
+    }}
+    public static void TestGetallstaff() throws Exception{try {
+        List <Staff> staffs = new ArrayList<>();
+        Connection con = DataBaseConnector.getConnection();
+        staffs=staffDAO.getAllStaffs(con);
+        for(int i = 0; i < staffs.size() ; i ++){
+            System.out.println(staffs.get(i).getFirstname());
+        }
+        con.close();
+
+    }catch (Exception ex){
+        ex.printStackTrace();
+    }
+    }
+    public static void TestEditstaff() throws Exception{try{
+        Staff staff = new Staff();
+        staff.setFirstname("xXx");
+        staff.setLastname("xXx");
+        staff.setMainPhoneNumber("xXx");
+        staff.setStaffStatus(StaffStatus.TEACHER);
+        staff.setAdditionalPhoneNumber("xXx");
+        staff.setEmail("xXx");
+        staff.setId(2);
+        Connection con = DataBaseConnector.getConnection();
+        staffDAO.editStaff(staff,con);
+        con.close();
+    }catch (Exception ex){
+        ex.printStackTrace();
+    }
+    }
+    public static void TestDeleteStaff() throws Exception{try{
+        Staff staff = new Staff();
+        staff.setId(2);
+        Connection con = DataBaseConnector.getConnection();
+        staffDAO.deleteStaff(staff,con);
+        con.close();
+    }catch(Exception ex){
+        ex.printStackTrace();
+    }
+
+    }
+}
--- src/main/java/ge/mziuri/dao/StaffDAOImpl.java
+++ src/main/java/ge/mziuri/dao/StaffDAOImpl.java
@@ -21,32 +21,33 @@ public class StaffDAOImpl implements StaffDAO {
         pstm.setString(5,staff.getAdditionalPhoneNumber());
         pstm.setString(6,staff.getPassword());
         pstm.setString(7,staff.getStaffStatus().name());
-        pstm.executeLargeUpdate();
+        pstm.executeUpdate();
         pstm.close();
         con.close();
     }
 
     @Override
     public void deleteStaff(Staff staff, Connection con) throws Exception {
-
+    PreparedStatement pstmt = con.prepareStatement("DELETE  FROM staff where id=?");
+    pstmt.setInt(1,staff.getId());
+    pstmt.executeUpdate();
+    pstmt.close();
+    con.close();
     }
 
     @Override
     public void editStaff(Staff staff, Connection con) throws Exception {
-        PreparedStatement pstmt1 = con.prepareStatement( "Update staff SET pnumber =? , pnumber2 =? , mail = ? , pass = ?  Where id=? ");
-        PreparedStatement pstmt2 = con.prepareStatement("SELECT * FROM staff WHERE id=?");
-        pstmt2.setInt(1,staff.getId());
-        pstmt1.setInt(5,staff.getId());
-        ResultSet rs = pstmt2.executeQuery();
-        rs.next();
-        if(staff.getMainPhoneNumber().isEmpty()) pstmt1.setInt(1, rs.getInt("mainphonenumber"));
-            else pstmt1.setString(1,staff.getMainPhoneNumber());
-        if(staff.getAdditionalPhoneNumber().isEmpty() ) pstmt1.setInt(2, rs.getInt("additionalphonenumber"));
-            else pstmt1.setString(2,staff.getAdditionalPhoneNumber());
-        if(staff.getEmail().isEmpty()) pstmt1.setString(3, rs.getString("email"));
-            else pstmt1.setString(3, staff.getEmail());
-        if(staff.getPassword().isEmpty()) pstmt1.setString(4,rs.getString("password"));
-            else pstmt1.setString(4,staff.getPassword());
+    PreparedStatement pstmt = con.prepareStatement("Update staff SET firstname=?, lastname=?, email=?, main_phone_number=?,additional_phone_number=?, staff_status=? WHERE id=?");
+    pstmt.setString(1,staff.getFirstname());
+    pstmt.setString(2, staff.getLastname());
+    pstmt.setString(3,staff.getEmail());
+    pstmt.setString(4,staff.getMainPhoneNumber());
+    pstmt.setString(5,staff.getAdditionalPhoneNumber());
+    pstmt.setString(6,staff.getStaffStatus().name());
+    pstmt.setInt(7,staff.getId());
+    pstmt.executeUpdate();
+    pstmt.close();
+    con.close();
     }
 
     @Override
@@ -58,8 +59,8 @@ public class StaffDAOImpl implements StaffDAO {
         int ID = rs.getInt("id");
         String name =rs.getString("firstname");
         String surname =rs.getString("lastname");
-        String pnumber1=rs.getString("mainphonenumber");
-        String pnumber2=rs.getString("additionalphonenumber");
+        String pnumber1=rs.getString("main_phone_number");
+        String pnumber2=rs.getString("additional_phone_number");
         String mail = rs.getString("email");
         Staff staff = new Staff();
         staff.setId(ID);
