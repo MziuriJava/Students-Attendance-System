23d230550e865ba2040c72365e903646fc3e8917
OtOoToOtO <otarphrodiashvili@gmail.com>
2018-04-01 13:05:38 +0400

    DisplayingStaffs servlet + jsp edited

--- src/main/java/ge/mziuri/servlet/LoadStaffServlet.java
+++ src/main/java/ge/mziuri/servlet/LoadStaffServlet.java
@@ -1,6 +1,29 @@
 package ge.mziuri.servlet;
 
-public class LoadStaffServlet {
+import ge.mziuri.dao.staff.StaffDAO;
+import ge.mziuri.dao.staff.StaffDAOImpl;
+import ge.mziuri.model.user.staff.Staff;
+import ge.mziuri.util.db.DataBaseConnector;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+public class LoadStaffServlet extends HttpServlet {
+
+    private StaffDAO staffDAO = new StaffDAOImpl();
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp){
+        try {
+            List<Staff> staffs = staffDAO.getAllStaffs(DataBaseConnector.getConnection());
+            req.setAttribute("staffs", staffs);
+            req.getRequestDispatcher("sas/admin/staffs.jsp").forward(req, resp);
+        } catch (Exception e) {
+            // TODO
+        }
+    }
+
 
 
 
