1bda1aef83828904efb3122a5bb061d24729b2c1
chapidzenika <chapidzenika2@gmail.com>
2018-04-21 17:28:39 +0400

    add course jsp

---
+++ src/main/java/ge/mziuri/servlet/AddCourseServlet.java
@@ -0,0 +1,10 @@
+package ge.mziuri.servlet;
+
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
+
+@MultipartConfig(maxFileSize = 16177215)
+public class AddCourseServlet extends HttpServlet {
+
+
+}
